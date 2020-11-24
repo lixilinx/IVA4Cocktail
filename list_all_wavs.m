9d25cc555cfc5e268b6fdb601eaf2d09e0a460ca
lixilinx <37510137+lixilinx@users.noreply.github.com>
2020-11-24 11:46:14 -0800

    Delete list_all_wavs.m

--- list_all_wavs.m
+++
@@ -1,8 +0,0 @@
-function list = list_all_wavs( dir0 )
-% list all wav files in a dir
-list = dir([dir0, '**']);
-filter = zeros(size(list));
-for j = 1 : length(list)
-    filter(j) = (~list(j).isdir) && (endsWith(list(j).name, '.wav') || endsWith(list(j).name, '.WAV'));
-end
-list = list(filter==1);
